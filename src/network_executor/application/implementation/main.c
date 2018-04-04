f474180245c5c584b62ea286c7ca8c407e6a57a3
Florian Schmidt <it.schmflo@gmail.com>
2018-04-05 00:17:20 +0200

    began cog code for network struct initialization

--- src/network_executor/application/implementation/main.c
+++ src/network_executor/application/implementation/main.c
@@ -1,10 +1,21 @@
 #include "testsession.h"
 
+//TODO initialize network structure based on net model
+/* [[[cog
+import cog
+from network_descriptor.NetInstance import net
+
+cog.outl("NeuronalNetwork_t network =")
+cog.outl("{")
 
 
+cog.outl("}")
+]]] */
+
+// [[[end]]]
 
 
 int main(void)
 {
-    exec_testsession(NeuronalNetwork_p network, DataSupplier_p supplier)
+    exec_testsession(network, supplier);
 }
